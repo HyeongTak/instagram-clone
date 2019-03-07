5e74bfabdf1fcf3100c86faf09b3b256d114c1ec
HyeongTak <andy395023@gmail.com>
2019-03-07 23:06:04 +0900

    CardComponent

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
-import { Icon } from 'native-base';
+import { Container, Content, Icon } from 'native-base';
+import CardComponent from '../CardComponent';
 
 export default class HomeTab extends Component {
 
@@ -13,9 +13,11 @@ export default class HomeTab extends Component {
 
     render() {
         return (
-            <View style={style.container}>
-                <Text>HomeTab</Text>
-            </View>
+            <Container style={style.container}>
+                <Content>
+                    <CardComponent />
+                </Content>
+            </Container>
         );
     }
 }
