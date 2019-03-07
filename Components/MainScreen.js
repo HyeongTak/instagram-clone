1aad9648a52761282c7352e3a581ae85661c49df
HyeongTak <andy395023@gmail.com>
2019-03-07 20:43:48 +0900

    탭 네비게이터 애니메이션

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Platform } from 'react-native';
 import { Icon } from 'native-base'; 
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation'; 
+import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'; 
 
 // 하단 탭에 들어갈 컴포넌트들
 import HomeTab from './AppTabNavigator/HomeTab'
@@ -10,12 +10,34 @@ import AddMediaTab from './AppTabNavigator/AddMediaTab'
 import LikesTab from './AppTabNavigator/LikesTab'
 import ProfileTab from './AppTabNavigator/ProfileTab'
 
-const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab },
-  SearchTab: { screen: SearchTab },
-  AddMediaTab: { screen: AddMediaTab },
-  LikesTab: { screen: LikesTab },
-  ProfileTab: { screen: ProfileTab }
+const AppTabNavigator = createMaterialTopTabNavigator({
+  HomeTab:{ screen: HomeTab },
+  Search:{ screen: SearchTab },
+  AddMedia:{ screen: AddMediaTab },
+  Likes:{ screen: LikesTab },
+  Profile:{ screen: ProfileTab }
+}, {
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        },
+        android:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    iconStyle: { height: 100 },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
 });
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
